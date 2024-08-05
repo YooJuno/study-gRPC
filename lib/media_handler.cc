b4a3f2b81e3eeda9e8d94de42e6dc067dbf51867
YooJuno <juno980220@naver.com>
2024-08-05 15:50:23 +0900

    src creation

---
+++ lib/media_handler.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include "remote_message.grpc.pb.h"
+#include "media_handler.h"
+
+using remote::ProtoMat;
+using namespace std;
+
+#define COLOR 3
+#define GRAY 1
+
+auto MediaHandler::ConvertProtomatToMat(const ProtoMat& protomat) -> cv::Mat
+{
+    cv::Mat img = cv::Mat(cv::Size(protomat.width(), protomat.height()), protomat.type());
+    string serializedMatrix(protomat.buffer());
+    int idx = 0;
+
+    uchar* data = img.data;
+
+    for (auto i=0 ; i<img.size().height ; i++)
+    {
+        for (auto j=0 ; j<img.size().width ; j++)
+        {  
+            if (protomat.channels() == GRAY)
+            {   
+                img.at<uchar>(i, j) = serializedMatrix[idx++];
+
+
+            }
+            else
+            {
+                img.at<cv::Vec3b>(i, j)[0] = serializedMatrix[idx++];
+                img.at<cv::Vec3b>(i, j)[1] = serializedMatrix[idx++];
+                img.at<cv::Vec3b>(i, j)[2] = serializedMatrix[idx++];
+
+
+            }
+        }
+    }
+
+    return img;
+}
+
+auto MediaHandler::ConvertMatToProtomat(const cv::Mat& image) -> ProtoMat
+{
+    ProtoMat result;
+    string buffer("");
+    
+    result.set_width(image.size().width);
+    result.set_height(image.size().height);
+    result.set_channels(image.channels());
+    result.set_type(image.type());
+    result.set_seq(result.seq() + 1);
+    
+    for (auto i=0 ; i<image.size().height ; i++)
+    {
+        for (auto j=0 ; j<image.size().width ; j++)
+        {
+            if (image.channels() == GRAY)
+            {       
+                buffer += static_cast<uchar>(image.at<uchar>(i, j));
+            }
+            else if (image.channels() == COLOR)
+            {
+                const cv::Vec3b& pixel = image.at<cv::Vec3b>(i, j);
+
+                buffer += static_cast<uchar>(pixel[0]); // B
+                buffer += static_cast<uchar>(pixel[1]); // G
+                buffer += static_cast<uchar>(pixel[2]); // R
+            }
+        }
+    }
+
+    result.set_buffer(buffer); 
+    
+    return result;
+}
